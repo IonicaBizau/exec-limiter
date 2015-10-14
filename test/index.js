f962e05f4906011839d28979aa79cbc5bef82780
Ionică Bizău <bizauionica@gmail.com>
2015-10-14 15:58:49 +0300

    Use mocha to run the tests

--- test/index.js
+++ test/index.js
@@ -1,70 +1,67 @@
-var ExecLimiter = require("../lib");
-var assert = require("assert");
+// Dependencies
+var ExecLimiter = require("../lib")
+  , assert = require("assert")
+  ;
 
+// Create the exec limiter
 var el = new ExecLimiter(10);
 
-describe("ExecLimiter", function() {
+describe("errors", function () {
+    var command = "ls";
 
-    describe("#add()", function() {
+    it("spawn: " + command + " - should finish gracefully", function (done) {
+        el.add(command, [], done);
+    });
 
-        describe("errors", function() {
+    it("exec: " + command + " - should finish gracefully", function (done) {
+        el.add(command, done);
+    });
+});
 
-            var command = "ls";
+describe("exec vs. spawn", function() {
+    // All tests must finish in less than 4 seconds
+    this.timeout(4000);
 
-            it("spawn: " + command + " - should finish gracefully", function(done) {
-                el.add(command, [], done);
-            });
+    var tts = 2
+      , command = "sleep"
+      ;
 
-            it("exec: " + command + " - should finish gracefully", function(done) {
-                el.add(command, done);
-            });
+    it("spawn: " + command + " " + tts + " - should wait at least " + tts + " seconds before it finishes", function(done) {
+        var start = Date.now();
+        el.add(command, [tts], function(err) {
+            var end = Date.now();
+            done(end - start < tts * 1000 ? "did not finish in more than " + tts + " seconds" : undefined);
         });
+    });
 
-        describe("exec vs. spawn", function() {
-            // all tests must finish in less than 4 seconds
-            this.timeout(4000);
-
-            var tts = 2;
-            var command = "sleep";
-
-            it("spawn: " + command + " " + tts + " - should wait at least " + tts + " seconds before it finishes", function(done) {
-                var start = Date.now();
-                el.add(command, [tts], function(err) {
-                    var end = Date.now();
-                    done(end - start < tts * 1000 ? "did not finish in more than " + tts + " seconds" : undefined);
-                });
-            });
-
-            it("exec: " + command + " " + tts + " - should wait at least " + tts + " seconds before it finishes", function(done) {
-                var start = Date.now();
-                el.add(command + " " + tts, function(err) {
-                    var end = Date.now();
-                    done(end - start < tts * 1000 ? "did not finish in more than " + tts + " seconds" : undefined);
-                });
-            });
+    it("exec: " + command + " " + tts + " - should wait at least " + tts + " seconds before it finishes", function(done) {
+        var start = Date.now();
+        el.add(command + " " + tts, function(err) {
+            var end = Date.now();
+            done(end - start < tts * 1000 ? "did not finish in more than " + tts + " seconds" : undefined);
         });
+    });
+});
 
-        describe("environment", function() {
-            // all tests must finish in less than 4 seconds
-            this.timeout(4000);
+describe("environment", function() {
 
-            var name = "TEST_VAR";
-            var value = "test_value";
-            var command = "printenv";
+    // All tests must finish in less than 4 seconds
+    this.timeout(4000);
 
-            it("spawn: " + command + " - should not return an error if variables are passed correctly", function(done) {
-                var env = {};
-                env[name] = value;
+    var name = "TEST_VAR"
+      , value = "test_value"
+      , command = "printenv"
+      ;
 
-                el.add(command, [name], { env: env }, done);
-            });
+    it("spawn: " + command + " - should not return an error if variables are passed correctly", function(done) {
+        var env = {};
+        env[name] = value;
+        el.add(command, [name], { env: env }, done);
+    });
 
-            it("exec: " + command + " - should not return an error if variables are passed correctly", function(done) {
-                var env = {};
-                env[name] = value;
-
-                el.add(command + " " + name, { env: env }, done);
-            });
-        });
+    it("exec: " + command + " - should not return an error if variables are passed correctly", function(done) {
+        var env = {};
+        env[name] = value;
+        el.add(command + " " + name, { env: env }, done);
     });
 });
