cde13a46a6b9755e69bf55eb112e98c7739f6ced
Ionică Bizău <bizauionica@gmail.com>
2015-10-15 14:12:55 +0300

    Added another example

--- example/index.js
+++ example/index.js
@@ -22,11 +22,18 @@ el.add("sleep 5", function (err) {
     console.log(err || "I was ran in parallel with the second process and finished fine.");
 });
 
+// #4
+el.add("ls", ["-l"], { ignoreStdout: false }, function (err, stdout) {
+    console.log(err || "The spawned 'ls -l' returned:\n" + stdout);
+});
+
 // These will be executed like below:
 //
-// Timeline: 0-1-2-3-4-5-6-7-8-9-10
+// Timeline: 0-1-2-3-4-5-6-7-8-9-10-11
+//
 //       #1: ==========
 //       #2: ==============
 //       #3:            ============
+//       #4:               ==
 //
 // Notice how they run in parallel, but not more than 2 in the same time.
